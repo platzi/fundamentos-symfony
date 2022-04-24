6384d14c7afdb3b5cafaf1cf9b0b9226992e8973
Italo Morales F <italomoralesf@MacBook-Pro-de-Italo.local>
2022-04-24 15:57:46 -0500

    Entity

---
+++ src/Entity/Comment.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentRepository::class)]
+class Comment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'text')]
+    private $body;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): self
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+}
